1eba950a24fa8956a04a9ca7c4e27a49c9b0477a
NasimSursh <suroshzahiri@gmail.com>
2024-02-09 14:47:40 +0100

    sub class of module1

---
+++ src/com/example/math/Addition.java
@@ -0,0 +1,11 @@
+package com.example.math;
+
+public class Addition {
+  
+  public int add(int a, int b) {
+    int result = a+b;
+    System.out.println("Addition result is "+ result);
+    return result;
+  }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module com.example.math {
+  exports com.example.math;
+  
+}
